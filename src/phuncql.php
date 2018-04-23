b05cd1d90442072fbf6ff023d01a8c61f21c29b5
Rik Meijer <rik@pulledbits.org>
2018-04-23 12:24:47 +0200

    use pdo-mock

--- src/phuncql.php
+++ src/phuncql.php
@@ -4,9 +4,11 @@ declare(strict_types=1);
 namespace pulledbits\phuncql;
 
 function parseQueries(string $rawQueries) : array {
-    return [function(\PDO $connection){
-        return ['col1' => null, 'col2' => null];
-    }, function(\PDO $connection){
+    return [function(\PDO $connection) : array {
+        $statement = $connection->prepare('SELECT col1, col2 FROM table');
+        $statement->execute();
+        return $statement->fetchAll();
+    }, function(\PDO $connection)  : array {
         return ['col3' => null, 'col2' => null];
     }];
 }
--- tests/PhuncqlTest.php
+++ tests/PhuncqlTest.php
@@ -3,6 +3,9 @@ declare(strict_types=1);
 
 namespace pulledbits\phuncql;
 
+use function pulledbits\pdomock\createMockPDOCallback;
+use function pulledbits\pdomock\createMockPDOStatementFetchAll;
+
 class PhuncqlTest extends \PHPUnit\Framework\TestCase
 {
     public function testSanity_When_True_Expect_True()
@@ -11,19 +14,27 @@ class PhuncqlTest extends \PHPUnit\Framework\TestCase
     }
 
     public function testParseQueries_When_StringPassed_Expect_NewFunctionList() {
-        $pdo = new class extends \PDO {
-            public function __construct()
-            {}
-        };
+        $pdo = createMockPDOCallback();
+        $pdo->callback(function(string $query, array $parameters) {
+            switch ($query) {
+                case 'SELECT col1, col2 FROM table':
+                    return createMockPDOStatementFetchAll(['col1' => null, 'col2' => null]);
+            }
+        });
         $queries = parseQueries('SELECT col1, col2 FROM table');
         $this->assertArrayHasKey('col1', $queries[0]($pdo));
     }
 
     public function testParseQueries_When_StringPassed_Expect_NewFunctionListWithAFunctionPerQuery() {
-        $pdo = new class extends \PDO {
-            public function __construct()
-            {}
-        };
+        $pdo = createMockPDOCallback();
+        $pdo->callback(function(string $query, array $parameters) {
+            switch ($query) {
+                case 'SELECT col1, col2 FROM table':
+                    return createMockPDOStatementFetchAll(['col1' => null, 'col2' => null]);
+                case 'SELECT col3, col2 FROM table':
+                    return createMockPDOStatementFetchAll(['col3' => null, 'col2' => null]);
+            }
+        });
         $queries = parseQueries('SELECT col1, col2 FROM table;\nSELECT col3, col2 FROM table;');
         $this->assertArrayHasKey('col1', $queries[0]($pdo));
         $this->assertArrayHasKey('col3', $queries[1]($pdo));
