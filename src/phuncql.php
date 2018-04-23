3e667e4aef50e6945a10c64204af8bd598c2083b
Rik Meijer <rik@pulledbits.org>
2018-04-23 12:47:17 +0200

    refactor parseQueries to contain all code

--- src/phuncql.php
+++ src/phuncql.php
@@ -3,16 +3,12 @@ declare(strict_types=1);
 
 namespace pulledbits\phuncql;
 
-function _parseQueries(array $rawQueries) : array {
+function parseQueries(string $rawQueries) : array {
     return array_map(function(string $rawQuery) {
         return function(\PDO $connection) use ($rawQuery) : array {
             $statement = $connection->prepare($rawQuery);
             $statement->execute();
             return $statement->fetchAll(\PDO::FETCH_ASSOC);
         };
-    }, $rawQueries);
-}
-
-function parseQueries(string $rawQueries) : array {
-    return _parseQueries(explode(';', $rawQueries));
+    }, explode(';', $rawQueries));
 }
