9dd047d0d185119fe8ca6ecf9f102e2d2f41f6e4
Rik Meijer <rik@pulledbits.org>
2018-04-23 13:47:46 +0200

    experimenting with a functional class using __invoke()

--- src/phuncql.php
+++ src/phuncql.php
@@ -5,17 +5,25 @@ namespace pulledbits\phuncql;
 
 use function iter\rewindable\map;
 
+spl_autoload_register(function(string $class) {
+    if (strpos($class, __NAMESPACE__) === false) {
+        return;
+    }
+    $subclass = substr($class, strlen(__NAMESPACE__));
+    $module = substr($subclass, strlen('\\'), strpos($subclass, '\\', 1) - 1);
+    switch ($module) {
+        case 'pdo':
+            require __DIR__ . DIRECTORY_SEPARATOR . 'pdo' . DIRECTORY_SEPARATOR . 'prepare.php';
+            break;
+    }
+    return;
+});
+
+
 function parseQueries(string $rawQueries) : iterable
 {
     $parseQuery = function (string $rawQuery) {
-        return function (\PDO $connection) use ($rawQuery) : array
-        {
-            $statement = $connection->prepare($rawQuery);
-            if ($statement->execute() === false) {
-                return [];
-            }
-            return $statement->fetchAll(\PDO::FETCH_ASSOC);
-        };
+        return new pdo\prepare($rawQuery);
     };
     return map($parseQuery, explode(';', $rawQueries));
 }
