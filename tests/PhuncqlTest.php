2dcb66ecda603d735832375922b75c27fabac794
Rik Meijer <rik@pulledbits.org>
2018-04-23 10:41:58 +0200

    initialized PHPUnit test

---
+++ tests/PhuncqlTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace pulledbits\phuncql;
+
+class PhuncqlTest extends \PHPUnit\Framework\TestCase
+{
+    public function testSanity_When_True_Expect_True()
+    {
+        $this->assertTrue(true);
+    }
+}
