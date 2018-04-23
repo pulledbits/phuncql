95be4019bb95435e2d23c37e264e21daac66dcc0
Rik Meijer <rik@pulledbits.org>
2018-04-23 16:14:23 +0200

    simplify autoloading

--- src/phuncql.php
+++ src/phuncql.php
@@ -5,30 +5,24 @@ namespace pulledbits\phuncql;
 
 use function iter\rewindable\map;
 
-function activateModule(string $moduleIdentifier) : void {
-    spl_autoload_register(moduleAutoloader(__NAMESPACE__ . '\\' . $moduleIdentifier, __DIR__ . DIRECTORY_SEPARATOR . $moduleIdentifier));
-    include_once __DIR__ . DIRECTORY_SEPARATOR . $moduleIdentifier . '.php';
-}
-
-function moduleAutoloader(string $moduleNamespace, string $modulePath) {
-    return function(string $class) use ($moduleNamespace, $modulePath) : void {
-        if (strpos($class, $moduleNamespace) !== false) {
+function loadModule(string $moduleNamespace, string $modulePath) : void {
+    spl_autoload_register(function(string $class) use ($moduleNamespace, $modulePath) : void {
+        if (strpos($class, $moduleNamespace) === 0) {
             include_once $modulePath . str_replace('\\', DIRECTORY_SEPARATOR, str_replace($moduleNamespace, '', $class)) . '.php';
         }
-    };
+    });
+    include_once $modulePath . '.php';
 }
 
-spl_autoload_register(function(string $class) {
-    if (strpos($class, __NAMESPACE__) === false) {
-        return;
-    }
-    $module = substr($class, strlen(__NAMESPACE__));
-    $nsSeparatorPosition = strpos($module, '\\', 1);
-    if ($nsSeparatorPosition === false) {
-        activateModule(substr($module, strlen('\\')));
-    }
-});
+function preloadModule(string $moduleIdentifier) {
+    spl_autoload_register(function(string $class) use ($moduleIdentifier) {
+        if (str_replace(__NAMESPACE__ . '\\', '', $class) === $moduleIdentifier) {
+            loadModule(__NAMESPACE__ . '\\' . $moduleIdentifier, __DIR__ . DIRECTORY_SEPARATOR . $moduleIdentifier);
+        }
+    });
+}
 
+preloadModule('pdo');
 
 function parseQueries(string $rawQueries) : iterable
 {
