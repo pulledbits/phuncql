4a2601d35835fff4a0fec122b36d9b92408b2d4c
Rik Meijer <rik@pulledbits.org>
2018-04-23 12:49:04 +0200

    extract parseQuery

--- src/phuncql.php
+++ src/phuncql.php
@@ -3,12 +3,14 @@ declare(strict_types=1);
 
 namespace pulledbits\phuncql;
 
+function parseQuery(string $rawQuery) {
+    return function(\PDO $connection) use ($rawQuery) : array {
+        $statement = $connection->prepare($rawQuery);
+        $statement->execute();
+        return $statement->fetchAll(\PDO::FETCH_ASSOC);
+    };
+}
+
 function parseQueries(string $rawQueries) : array {
-    return array_map(function(string $rawQuery) {
-        return function(\PDO $connection) use ($rawQuery) : array {
-            $statement = $connection->prepare($rawQuery);
-            $statement->execute();
-            return $statement->fetchAll(\PDO::FETCH_ASSOC);
-        };
-    }, explode(';', $rawQueries));
+    return array_map(__NAMESPACE__ . '\\parseQuery', explode(';', $rawQueries));
 }
