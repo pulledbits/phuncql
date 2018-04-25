30d1741b32a19ac5e2e044ba21c7c31b43140cc6
Rik Meijer <rik@pulledbits.org>
2018-04-25 14:27:21 +0200

    test "parsing" multiple queries

--- tests/PhuncqlTest.php
+++ tests/PhuncqlTest.php
@@ -17,5 +17,12 @@ class PhuncqlTest extends \PHPUnit\Framework\TestCase
         $this->assertEquals('SELECT col1, col2 FROM table', $queries[0]);
     }
 
+    public function testParseQueries_When_MulitpleQueries_Expect_ElementsToContainSucceedingQueries() {
+        $queries = phuncql::parseQueries('SELECT col1, col2 FROM table;SELECT col3, col4 FROM table;SELECT col5, col6 FROM table');
+
+        $this->assertEquals('SELECT col1, col2 FROM table', $queries[0]);
+        $this->assertEquals('SELECT col3, col4 FROM table', $queries[1]);
+        $this->assertEquals('SELECT col5, col6 FROM table', $queries[2]);
+    }
 
 }
