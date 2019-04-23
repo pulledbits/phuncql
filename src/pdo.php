87acdb334f2a584b9d9dd79169c5b1022ca4007f
Rik Meijer <rik@pulledbits.org>
2019-04-23 10:56:44 +0200

    make more functional by dropping helper classes

--- src/pdo.php
+++ src/pdo.php
@@ -4,7 +4,24 @@ namespace pulledbits\phuncql;
 
 class pdo
 {
-    static function connect(\PDO $connection) : pdo\connect {
-        return new pdo\connect($connection);
+    static function connect(\PDO $connection) : \Closure{
+        return function(string $rawQuery) use ($connection) : \Closure {
+            return self::prepare($connection->prepare($rawQuery));
+        };
+    }
+    static function prepare(\PDOStatement $statement) : \Closure {
+        return function (...$functionArguments) use ($statement) : array {
+            if (count($functionArguments) === 0) {
+                $result = $statement->execute();
+            } elseif (is_array($functionArguments[0])) {
+                $result = $statement->execute($functionArguments[0]);
+            } else {
+                $result = $statement->execute($functionArguments);
+            }
+            if ($result) {
+                return $statement->fetchAll(\PDO::FETCH_ASSOC);
+            }
+            return [];
+        };
     }
 }
