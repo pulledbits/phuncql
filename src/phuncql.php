35e9cca65cad746344c3d33a759c000b995ff24d
Rik Meijer <rik@pulledbits.org>
2019-04-23 20:42:44 +0200

    drop phuncql class

--- src/phuncql.php
+++ src/phuncql.php
@@ -3,15 +3,7 @@ declare(strict_types=1);
 
 namespace pulledbits\phuncql;
 
-class phuncql
+function connect(string $dsn, callable $error) : \Closure
 {
-    public static function parseQueries(string $rawQueries) : iterable
-    {
-        return explode(';', $rawQueries);
-    }
-
-    public static function connect(string $dsn, callable $error) : \Closure
-    {
-        return pdo::connect($dsn, $error);
-    }
+    return pdo::connect($dsn, $error);
 }
--- tests/PhuncqlTest.php
+++ tests/PhuncqlTest.php
@@ -13,31 +13,17 @@ class PhuncqlTest extends \PHPUnit\Framework\TestCase
         $this->assertTrue(true);
     }
 
-    public function testParseQueries_When_OneQuery_Expect_FirstElementToContainQuery() : void  {
-        $queries = phuncql::parseQueries('SELECT col1, col2 FROM table');
-
-        $this->assertEquals('SELECT col1, col2 FROM table', $queries[0]);
-    }
-
-    final public function testParseQueries_When_MulitpleQueries_Expect_ElementsToContainSucceedingQueries() : void {
-        $queries = phuncql::parseQueries('SELECT col1, col2 FROM table;SELECT col3, col4 FROM table;SELECT col5, col6 FROM table');
-
-        $this->assertEquals('SELECT col1, col2 FROM table', $queries[0]);
-        $this->assertEquals('SELECT col3, col4 FROM table', $queries[1]);
-        $this->assertEquals('SELECT col5, col6 FROM table', $queries[2]);
-    }
-
     final public function testConnect_When_ValidDSN_Expect_QueryExecutorClosure() : void {
         pdo::$links['mysql:host=localhost;dbname=testdb'] = createMockPDOCallback();
         pdo::$links['mysql:host=localhost;dbname=testdb']->callback(function(string $query, array $parameters) {
         });
-        $querier = phuncql::connect('mysql:host=localhost;dbname=testdb', function(\Error $error){});
+        $querier = connect('mysql:host=localhost;dbname=testdb', function(\Error $error){});
         $this->assertInstanceOf('Closure', $querier);
         unset(pdo::$links['mysql:host=localhost;dbname=testdb']);
     }
 
     final public function testConnect_When_ConnectionError_Expect_LogTriggered() : void {
-        $querier = phuncql::connect('mysql:host=localhost;dbname=testdb', function(\Error $error) {
+        $querier = connect('mysql:host=localhost;dbname=testdb', function(\Error $error) {
             $this->assertContains('Unable to connect', $error->getMessage());
         });
     }
@@ -48,7 +34,7 @@ class PhuncqlTest extends \PHPUnit\Framework\TestCase
         pdo::$links['mysql:host=localhost;dbname=testdb'] = createMockPDOCallback();
         pdo::$links['mysql:host=localhost;dbname=testdb']->callback(function(string $query, array $parameters) {
         });
-        $querier = phuncql::connect('mysql:host=localhost;dbname=testdb', function(\Error $error){});
+        $querier = connect('mysql:host=localhost;dbname=testdb', function(\Error $error){});
         $query = $querier('SELECT * FROM test');
         $this->assertInstanceOf('Closure', $query());
         unset(pdo::$links['mysql:host=localhost;dbname=testdb']);
