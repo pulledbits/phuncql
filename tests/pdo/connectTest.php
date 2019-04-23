648093e23c6a2c7b8373730e4e5f92dc88dd5f66
Rik Meijer <rik@pulledbits.org>
2019-04-23 14:14:18 +0200

    enhance all uniqid calls

--- tests/pdo/connectTest.php
+++ tests/pdo/connectTest.php
@@ -33,8 +33,8 @@ class connectTest extends TestCase
 
     public function test__invoke_When_NamedPlaceholdersInQuery_Expect_RequiredParameters()
     {
-        $col3Identifier = uniqid();
-        $col3Value = uniqid();
+        $col3Identifier = uniqid("invoke", true);
+        $col3Value = uniqid("invoke", true);
 
         $pdo = createMockPDOCallback();
         $pdo->callback(function(string $query, array $parameters) use ($col3Identifier, $col3Value) {
@@ -54,8 +54,8 @@ class connectTest extends TestCase
 
     public function test__invoke_When_PlaceholdersInQuery_Expect_RequiredParameters()
     {
-        $col3Identifier = uniqid();
-        $col3Value = uniqid();
+        $col3Identifier = uniqid("invoke", true);
+        $col3Value = uniqid("invoke", true);
 
         $pdo = createMockPDOCallback();
         $pdo->callback(function(string $query, array $parameters) use ($col3Identifier, $col3Value) {
@@ -74,8 +74,8 @@ class connectTest extends TestCase
 
     public function test__invoke_When_PlaceholdersInQueryAndParametersGiven_Expect_ResultSet()
     {
-        $col3Identifier = uniqid();
-        $col3Value = uniqid();
+        $col3Identifier = uniqid("invoke", true);
+        $col3Value = uniqid("invoke", true);
 
         $pdo = createMockPDOCallback();
         $pdo->callback(function(string $query, array $parameters) use ($col3Identifier, $col3Value) {
@@ -96,11 +96,8 @@ class connectTest extends TestCase
 
     public function test__invoke_When_InvalidQuery_Expect_FailedExecution()
     {
-        $col3Identifier = uniqid();
-        $col3Value = uniqid();
-
         $pdo = createMockPDOCallback();
-        $pdo->callback(function(string $query, array $parameters) use ($col3Identifier, $col3Value) {
+        $pdo->callback(function(string $query, array $parameters) {
             switch ($query) {
                 case 'SELECT col1, col2 FROM table':
                     return createMockPDOStatement($query, false);
